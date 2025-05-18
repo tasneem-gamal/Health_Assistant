3039e31c09aac6ff2d0ddf9c94acd0729cd24929
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-17 19:24:57 -0700

    setup structure

---
+++ lib/health_assistant.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:health_assistant/presentation/view/screens/on_boarding/on_boarding_view.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+class HealthAssistant extends StatelessWidget {
+  const HealthAssistant({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+        child: child!
+      ),
+      themeMode: ThemeMode.light,
+      home: const OnBoardingView(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:health_assistant/health_assistant.dart';
 
-import 'package:health_assistant/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HealthAssistant());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
