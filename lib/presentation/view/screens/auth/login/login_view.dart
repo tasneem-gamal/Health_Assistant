42eb3007572f426c292c4ccac6fd3d485b358207
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-18 20:14:49 -0700

    refactor

--- lib/presentation/view/screens/auth/login/login_view.dart
+++ lib/presentation/view/screens/auth/login/login_view.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:health_assistant/core/theming/styles.dart';
 import 'package:health_assistant/core/utils/constants.dart';
 import 'package:health_assistant/core/utils/spacing.dart';
-import 'package:health_assistant/core/widgets/custom_app_button.dart';
-import 'package:health_assistant/core/widgets/custom_text_form_field.dart';
+import 'package:health_assistant/presentation/view/widgets/auth/login/login_form.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -30,36 +29,7 @@ class LoginViewBody extends StatelessWidget {
             verticalSpace(context, 70),
             Center(child: Image.asset('assets/images/splash.png')),
             verticalSpace(context, 80),
-            Text(
-              'Login to Your Account',
-              style: CustomTextStyles.font14WhiteBold(context).copyWith(color: Colors.black),
-            ),
-            verticalSpace(context, 20),
-            CustomTextFormField(
-              validator: (validator){}, 
-              hintText: 'Username or email'
-            ),
-            verticalSpace(context, 20),
-            CustomTextFormField(
-              validator: (validator){}, 
-              hintText: 'Password',
-              suffixIcon: const Icon(Icons.visibility_off),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-              onPressed: () {},
-              child: Text(
-                'Forgot Password?',
-                style: CustomTextStyles.font12BlackMedium(context),
-              )),
-            ),
-            verticalSpace(context, 24),
-            CustomAppButton(
-              btnText: 'Login',
-              onPressed: (){},
-            ),
-            verticalSpace(context, 30),
+            const LoginForm(),
             Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -82,3 +52,4 @@ class LoginViewBody extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/presentation/view/widgets/auth/login/login_form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:health_assistant/core/theming/styles.dart';
+import 'package:health_assistant/core/utils/spacing.dart';
+import 'package:health_assistant/core/widgets/custom_app_button.dart';
+import 'package:health_assistant/core/widgets/custom_text_form_field.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: GlobalKey(),
+      autovalidateMode: AutovalidateMode.disabled,
+      child: Column(
+        children: [
+          Text(
+            'Login to Your Account',
+            style: CustomTextStyles.font14WhiteBold(context).copyWith(color: Colors.black),
+          ),
+          verticalSpace(context, 20),
+          CustomTextFormField(
+            validator: (validator){}, 
+            hintText: 'Username or email'
+          ),
+          verticalSpace(context, 20),
+          CustomTextFormField(
+            validator: (validator){}, 
+            hintText: 'Password',
+            suffixIcon: const Icon(Icons.visibility_off),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Forgot Password?',
+              style: CustomTextStyles.font12BlackMedium(context),
+            )),
+          ),
+          verticalSpace(context, 24),
+          CustomAppButton(
+            btnText: 'Login',
+            onPressed: (){},
+          ),
+          verticalSpace(context, 30),
+        ],
+      ),
+    );
+  }
+}
