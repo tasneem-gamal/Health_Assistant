29e46fee075cfbdbbd4e8799f45ce601d3a5dbb9
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-19 19:13:49 -0700

    firebase configure

---
+++ lib/presentation/view/screens/home/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeViewBody(),
+    );
+  }
+}
+
+class HomeViewBody  extends StatelessWidget {
+  const HomeViewBody ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
