987264d0d5fce552023685a8e5315747644aebd4
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-25 09:45:49 -0700

    refactor

--- lib/presentation/view/widgets/auth/sign_up/signup_bloc_listner.dart
+++ lib/presentation/view/widgets/auth/sign_up/signup_bloc_listner.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:health_assistant/core/theming/colors.dart';
 import 'package:health_assistant/core/theming/styles.dart';
 import 'package:health_assistant/core/utils/extensions.dart';
 import 'package:health_assistant/core/widgets/custom_loading_indicator.dart';
 import 'package:health_assistant/presentation/controllers/auth/auth_cubit.dart';
+import 'package:health_assistant/presentation/view/screens/auth/login/login_view.dart';
 
 class SignupBlocListner extends StatelessWidget {
   const SignupBlocListner({super.key});
@@ -12,55 +12,35 @@ class SignupBlocListner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocListener<AuthCubit, AuthState>(
-      listenWhen: (previous, current) => 
-      current is AuthLoading || 
-      current is AuthSuccess || 
-      current is AuthFailure,
+      listenWhen: (previous, current) =>
+          current is AuthLoading ||
+          current is AuthSuccess ||
+          current is AuthFailure,
       listener: (context, state) {
-        if(state is AuthLoading){
+        if (state is AuthLoading) {
           showDialog(
-            context: context, 
-            builder: (context) => const CustomLoadingIndicator()
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => const CustomLoadingIndicator(),
+          ); 
+        } else if (state is AuthSuccess) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Signup Successful!'),
+            ),
           );
-        } else if (state is AuthSuccess){
-          showSuccessDialog(context, 'Signup Successful', 'Congratulations, you have signed up successfully!');
-        } else if(state is AuthFailure){
+          final navigator = Navigator.of(context);
+          Future.delayed(const Duration(seconds: 2), () {
+            navigator.pushReplacement(
+              MaterialPageRoute(builder: (_) => const LoginView()),
+            );
+          });
+        } else if (state is AuthFailure) {
           setupErrorState(context, state.error);
         }
       },
       child: const SizedBox.shrink(),
     );
-  }
-
-      dynamic showSuccessDialog(BuildContext context, String sucessMessage, String message ) {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text(sucessMessage),
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: [
-                  Text(message),
-                ],
-              ),
-            ),
-            actions: [
-              TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: ColorsManager.mainColor,
-                  disabledForegroundColor: Colors.grey.withOpacity(0.38),
-                ),
-                onPressed: () {
-                  context.pop();
-                },
-                child: const Text('Continue'),
-              )
-            ],
-          );
-        }
-      );
   }
 
       void setupErrorState(BuildContext context, String error) {
