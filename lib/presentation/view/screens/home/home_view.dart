2933c4ec9283add0d49dd06f4af298b9975ca84d
tasneem-gamal <tasneemgamal044@gmail.com>
2025-05-25 11:48:49 -0700

    create home appbar

--- lib/presentation/view/screens/home/home_view.dart
+++ lib/presentation/view/screens/home/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:health_assistant/core/theming/colors.dart';
+import 'package:health_assistant/core/theming/styles.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,6 +8,32 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorsManager.mainColor,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: (){},
+          icon: const Icon(Icons.sort, color: Colors.white,),
+        ),
+        title: Text(
+          'Health Assistant',
+            style: CustomTextStyles.font24BlackMedium(context).copyWith(
+              foreground: Paint()
+                ..shader = const LinearGradient(
+                  colors: <Color>[
+                    Colors.white,
+                    ColorsManager.mainColorLight
+                  ],
+                ).createShader(const Rect.fromLTWH(200.0, 0.0, 50.0, 0.0)),
+            )),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(Icons.settings_outlined, color: Colors.white,),
+          )
+        ],
+      ),
       body: HomeViewBody(),
     );
   }
